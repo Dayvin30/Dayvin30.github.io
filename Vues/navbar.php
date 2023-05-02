80e1cfa71fcbb6eef40e6fa54cbb5acd6f86a7f4
Dayvin30 <92032342+Dayvin30@users.noreply.github.com>
2023-05-02 14:16:27 +0200

    set up project

---
+++ Vues/navbar.php
@@ -0,0 +1,9 @@
+<nav>
+    <ul>
+        <li><a href="index.php">Accueil</a></li>
+        <li><a href="connexion.php">Se connecter</a></li>
+        <li><a href="presentation.php">Présentation de l'entreprise</a></li>
+        <li><a href="catalogue.php">Catalogue des prestations</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page d'accueil - Ventalis</title>
+</head>
+<body>
+<?php include 'navbar.php'; ?>
+
+<!-- Contenu de la page d'accueil ici -->
+<h1>Bienvenue sur le site de Ventalis</h1>
+<p>Texte d'introduction ici</p>
+</body>
+</html>
